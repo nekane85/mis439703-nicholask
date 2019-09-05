58e16a4f13c17e9018127a88283edfa6940ede6c
Nick K <nicholas.e.kane@gmail.com>
2019-09-05 19:10:49 +0000

    First commit

---
+++ Assignments/Ruby/ex6.rb
@@ -0,0 +1,20 @@
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+#declares variables above that are called by X and Y
+puts x
+puts y
+#calls the variables and prints them on screen
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+#concattinating the declared variables into a string statement
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#declared more variables for use in evaluation following
+puts joke_evaluation
+#executes evaluations
+w = "This is the left side of..."
+e = "a string with a right side."
+#last declaration of variables and called and concattinated
+puts w + e
